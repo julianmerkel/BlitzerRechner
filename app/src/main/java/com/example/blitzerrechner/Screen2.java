41de70e3c4afb3ee99522922cf663608885cb44b
Jakob <j.simang@sap.com>
2019-10-07 17:47:26 +0200

    neue Felder und ganz viel Bums

--- app/src/main/java/com/example/blitzerrechner/Screen2.java
+++ app/src/main/java/com/example/blitzerrechner/Screen2.java
@@ -10,7 +10,9 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
@@ -19,8 +21,8 @@ public class Screen2 extends AppCompatActivity {
     Button berechnen;
     TextView textView;
     Intent intent3;
-    TextInputEditText inputErlaubteGeschw;
-    TextInputEditText inputGefahreneGeschw;
+    TextInputLayout inputErlaubteGeschw;
+    TextInputLayout inputGefahreneGeschw;
     Switch probezeit;
     Switch toleranz;
     ArrayList<String> parameter;
@@ -31,11 +33,13 @@ public class Screen2 extends AppCompatActivity {
         setContentView(R.layout.screen2);
         intent3 = new Intent(this, Screen3.class);
         textView = findViewById(R.id.textView3);
-        berechnen=findViewById(R.id.button);
-        inputErlaubteGeschw = findViewById(R.id.textInputEditText3);
-        inputGefahreneGeschw = findViewById(R.id.textInputEditText4);
-        probezeit = findViewById(R.id.switch1);
-        toleranz = findViewById(R.id.switch3);
+        berechnen = findViewById(R.id.buttonBerechnung);
+        inputErlaubteGeschw = findViewById(R.id.inputErlaubteGeschwindigkeit);
+        inputGefahreneGeschw = findViewById(R.id.inputGefahreneGeschwindigkeit);
+        // editLayout = findViewById(R.id.inputGefahreneGeschwindigkeit);
+        // editText = new TextInputEditText(editLayout.getContext());
+        probezeit = findViewById(R.id.switchProbezeit);
+        toleranz = findViewById(R.id.switchToleranz);
 
 
 
@@ -43,10 +47,11 @@ public class Screen2 extends AppCompatActivity {
         parameter = intent.getStringArrayListExtra("parameter");
 
 
-        berechnen.setOnClickListener(new View.OnClickListener(){
+     /*   berechnen.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                String erlaubt = inputErlaubteGeschw.getText().toString();
-                String gefahren = inputGefahreneGeschw.getText().toString();
+                String erlaubt = inputErlaubteGeschw.getEditText().getText().toString();
+                String gefahren = inputGefahreneGeschw.getEditText().getText().toString();
+                // String gefahren = editLayout.getEditText().getText().toString();
                 String sProbezeit = probezeit.isChecked()+"";
                 String sToleranz = toleranz.isChecked()+"";
                 parameter.set(1,erlaubt);
@@ -57,7 +62,69 @@ public class Screen2 extends AppCompatActivity {
                 intent3.putStringArrayListExtra("parameter", parameter);
                 startActivity(Screen2.this.intent3);
             }
-        });
+        });  */
 
+    }
+
+    private boolean validateGefahreneGeschw(){
+        String gefahren = inputGefahreneGeschw.getEditText().getText().toString().trim();
+        if(gefahren.isEmpty()){
+            inputGefahreneGeschw.setError("Dieses Feld darf nicht leer sein.");
+            return false;
+        }else if(inputGefahreneGeschw.getEditText().getText().toString().trim().length() > 3){
+            inputGefahreneGeschw.setError("Dies ist eine unrealistische Geschwindigkeit.");
+            return false;
+        }else{
+            inputGefahreneGeschw.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateErlaubteGeschw(){
+        String erlaubt = inputErlaubteGeschw.getEditText().getText().toString().trim();
+        if(erlaubt.isEmpty()){
+            inputErlaubteGeschw.setError("Dieses Feld darf nicht leer sein.");
+            return false;
+        }else if(inputErlaubteGeschw.getEditText().getText().toString().trim().length() > 3){
+            inputErlaubteGeschw.setError("Dies ist eine unrealistische Geschwindigkeit.");
+            return false;
+        }else{
+            inputErlaubteGeschw.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v){
+        if(!validateErlaubteGeschw() | !validateGefahreneGeschw()){
+            return;
+        }else{
+            String erlaubt = inputErlaubteGeschw.getEditText().getText().toString();
+            String gefahren = inputGefahreneGeschw.getEditText().getText().toString();
+            // String gefahren = editLayout.getEditText().getText().toString();
+            String sProbezeit = probezeit.isChecked()+"";
+            String sToleranz = toleranz.isChecked()+"";
+            parameter.set(1,erlaubt);
+            parameter.set(2,gefahren);
+            parameter.set(3,sProbezeit);
+            parameter.set(4,sToleranz);
+
+            intent3.putStringArrayListExtra("parameter", parameter);
+            startActivity(Screen2.this.intent3);
+        }
+    }
+
+    public void saveInput(View v){
+        String erlaubt = inputErlaubteGeschw.getEditText().getText().toString();
+        String gefahren = inputGefahreneGeschw.getEditText().getText().toString();
+        // String gefahren = editLayout.getEditText().getText().toString();
+        String sProbezeit = probezeit.isChecked()+"";
+        String sToleranz = toleranz.isChecked()+"";
+        parameter.set(1,erlaubt);
+        parameter.set(2,gefahren);
+        parameter.set(3,sProbezeit);
+        parameter.set(4,sToleranz);
+
+        intent3.putStringArrayListExtra("parameter", parameter);
+        startActivity(Screen2.this.intent3);
     }
 }
